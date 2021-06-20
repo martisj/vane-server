d04cdbaacba4ad1cffbdc265ff365047d5cc09fb
Martin Sjåstad <m.sjastad@gmail.com>
2021-06-20 14:19:12 +0200

    Add logging endpoint

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import Fastify from 'fastify'
 import groq from 'groq'
 import { client } from './sanityClient.js'
 import cors from 'fastify-cors'
+import { nanoid } from 'nanoid'
+import dayjs from 'dayjs'
 
 const server = Fastify({ logger: true })
 server.register(cors, { origin: ['http://localhost:3000', 'http://localhost:3001'], credentials: true, optionsSuccessStatus: 200 })
 
-const vanesQuery = groq`*[_type == 'vane'] | order(_createdAt desc)`
+const vanesQuery = groq`*[_type == 'vane' && !(_id in path('drafts.**'))] | order(_createdAt desc)`
 
 server.get('/vanes', async function () {
   const vanes = await client.fetch(vanesQuery)
@@ -54,6 +56,41 @@ server.delete('/vane/:id', {
   } catch (e) {
     reply.status(404)
     return { error: 'not found' }
+  }
+})
+
+server.post('/vane/log', {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['vaneId', 'day'],
+      additionalProperties: false,
+      properties: {
+        vaneId: { type: 'string' },
+        day: { type: 'string' }
+      }
+    }
+  },
+  response: {
+    200: {}
+  }
+},
+async function (request, reply) {
+  const timestamp = dayjs()
+  const { vaneId, day } = request.body
+  const date = dayjs(day)
+  try {
+    const update = await client.patch(vaneId).setIfMissing({ log: [] }).append(
+      'log', [{ _key: nanoid(), timestamp: timestamp.toISOString(), day: date.format('YYYY-MM-DD') }]
+    ).commit()
+    console.log(update)
+    return { vaneId, message: 'logged' }
+  } catch (e) {
+    console.error(e)
+    reply.status(400)
+    return {
+      error: "Can't track vane"
+    }
   }
 })
 
